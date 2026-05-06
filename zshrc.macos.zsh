0d90c7e1f2b58664df1fdea78fa45303116d91de
Gavin <flynnnnnlee@gmail.com>
2026-05-06 16:22:26 +0800

    Add Homebrew installation and Starship initialization

--- zshrc.macos.zsh
+++ zshrc.macos.zsh
@@ -52,6 +52,11 @@ unset_proxy() {
 
 
 # ============================================================
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# ============================================================
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# ============================================================
 # brew install starship
 # ============================================================
 eval "$(starship init zsh)"
