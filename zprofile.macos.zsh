abcfce5caa12b67bfd739fd97964306129aaf23e
Gavin <flynnnnnlee@gmail.com>
2026-05-06 12:53:06 +0800

    Create zprofile.macos.zsh for Homebrew setup
    Add macOS Zsh profile for Homebrew installation and environment setup.

---
+++ zprofile.macos.zsh
@@ -0,0 +1,6 @@
+# File path: $HOME/.zprofile
+
+# ============================================================
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# ============================================================
+eval "$(/opt/homebrew/bin/brew shellenv)"
