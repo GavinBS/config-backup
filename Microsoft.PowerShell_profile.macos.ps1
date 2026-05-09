1fa64a715b52c4adacf59daa05ca06b2696fafb9
Gavin <flynnnnnlee@gmail.com>
2026-05-09 17:04:51 +0800

    Create Microsoft.PowerShell_profile.macos.ps1

---
+++ Microsoft.PowerShell_profile.macos.ps1
@@ -0,0 +1,14 @@
+
+Invoke-Expression (&starship init powershell)
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+function y {
+	$tmp = (New-TemporaryFile).FullName
+	yazi @args --cwd-file="$tmp"
+	$cwd = Get-Content -Path $tmp -Encoding UTF8
+	if ($cwd -and $cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+		Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+	}
+	Remove-Item -Path $tmp
+}
